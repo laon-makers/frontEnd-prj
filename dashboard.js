a3a1e7a277ea879ca3319b5339bbc5a80af4d082
laon makers <chun-g-marwoom@laonmakers.com>
2021-12-23 00:10:21 -0500

    Version 1.0: 1) Added a pie chart. 2) Project status can be updated with project.js file.

--- dashboard.js
+++ dashboard.js
@@ -1,25 +1,28 @@
-var xPrjNames = [
-    "Dashbord",
-    "Game",
-    "IOT",
-    "Bug Tracker",
-    "Blog",
-    "Financial Portal",
-    "Inventory Control",
-    "Work Order System",
+let xPrjNames = [];
+let yPrjProgress = [];
+let prjChartColors = [];
+let yPrjHours = [];
+
+let xWorkTypeNames = ["Design", "Development", "Test & Debug", "Change Request", "Customer Service"];
+let yWorkTypeHours = [];
+let wTypebarColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
 ];
-var yPrjProgress = [100, 70, 50, 30, 20, 60, 15, 30]; //,20,10,0];
-//var prjBarColors = ["red", "green","blue","orange","brown", "purple", "cyan", "magenta"];
-var prjBarColors = [
-    "red",
-    "orange",
-    "cyan",
-    "green",
-    "brown",
-    "blue",
-    "navy",
-    "purple",
-];
+
+
+for( let i = 0; i < projects.length; i++ ) {
+    xPrjNames[i] = projects[i].name;
+    prjChartColors[i] = projects[i].color;
+    yPrjProgress[i] = projects[i].progress;
+    //yPrjHours[i] = projects[i].hours;
+    yPrjHours[i] = getProjectWorkHours(i);
+}
+
+populateWorkTypeHours(xPrjNames[0]);
 
 let dashbdChart = new Chart("dashboardChart", {
     type: "bar",
@@ -27,7 +30,7 @@ let dashbdChart = new Chart("dashboardChart", {
         labels: xPrjNames,
         datasets: [
             {
-                backgroundColor: prjBarColors,
+                backgroundColor: prjChartColors,
                 data: yPrjProgress,
             },
         ],
@@ -41,33 +44,70 @@ let dashbdChart = new Chart("dashboardChart", {
     },
 });
 
-function chgPrjColor() {
-    let i = 0;
-    tmp = prjBarColors[prjBarColors.length - 1];
-    for (i = prjBarColors.length - 1; i > 0; i--) {
-        prjBarColors[i] = prjBarColors[i - 1];
+let prjHourChart = new Chart("projectHourChart", {
+    type: "doughnut",
+    data: {
+      labels: xPrjNames,
+      datasets: [{
+        backgroundColor: prjChartColors,
+        data: yPrjHours
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Each Project's Work Hours"
+      }
+    }
+  });
+
+  let wTypeChart = new Chart("workTypeChart", {
+    type: "pie",
+    data: {
+      labels: xWorkTypeNames,
+      datasets: [{
+        backgroundColor: wTypebarColors,
+        data: yWorkTypeHours
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Hours of Dashboard Project's Work Type"
+      }
+    }
+  });
+
+function getProjectWorkHours(prjIx) {
+    let sum = 0;
+    
+    for( let i = 0; i < xWorkTypeNames.length; i++) {
+        sum += projects[prjIx][xWorkTypeNames[i]];
     }
 
-    prjBarColors[i] = tmp;
-    dashbdChart.update();
+    return sum;
 }
 
-function confuseProgress() {
-    let i = 0;
-    tmp = yPrjProgress[yPrjProgress.length - 1];
-    for (i = yPrjProgress.length - 1; i > 0; i--) {
-        yPrjProgress[i] = yPrjProgress[i - 1];
+function populateWorkTypeHours(prj) {
+    let ix, sum = 0;
+    projects.forEach(function (item, index) {
+        if(item.name == prj) ix = index;
+    });
+
+
+    for( let i = 0; i < xWorkTypeNames.length; i++) {
+        yWorkTypeHours[i] = projects[ix][xWorkTypeNames[i]];
+        sum += yWorkTypeHours[i];
     }
-
-    yPrjProgress[i] = tmp;
-    dashbdChart.update();
+    document.getElementById('workHours').children[0].textContent = sum;
 }
 
-function chgProgress() {
+
+  function updateProgress() {
     let sel = document.getElementById('selPrj');
     let newVal = document.getElementById('newProgress');
     let ix = sel.selectedIndex - 1;
-    if( ix < 8 ) {
+    if( ix < xPrjNames.length ) {
         yPrjProgress[ix] =  parseInt(newVal.value.trim());
 
         dashbdChart.update();
@@ -76,46 +116,129 @@ function chgProgress() {
     }
 }
 
-
-var xWorkTypeNames = ["Design", "Development", "Test & Debug", "Change Request", "Customer Service"];
-var yWorkTypeHours = [55, 49, 44, 24, 15];
-var wTypebarColors = [
-  "#b91d47",
-  "#00aba9",
-  "#2b5797",
-  "#e8c3b9",
-  "#1e7145"
-];
-
-let wTypeChart = new Chart("workTypeChart", {
-  type: "doughnut",
-  data: {
-    labels: xWorkTypeNames,
-    datasets: [{
-      backgroundColor: wTypebarColors,
-      data: yWorkTypeHours
-    }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: "Work Types and Hours"
+function chgPrjColor() {
+    let i = 0;
+    tmp = prjChartColors[prjChartColors.length - 1];
+    for (i = prjChartColors.length - 1; i > 0; i--) {
+        prjChartColors[i] = prjChartColors[i - 1];
     }
-  }
-});
 
+    prjChartColors[i] = tmp;
+    dashbdChart.update();
+}
 
+function shiftProjectStatus(chartIdName) {
+    let i = xPrjNames.length - 1;
+    tmpPrj = xPrjNames[i];
+    tmpY = yPrjProgress[i];
+    tmpC = prjChartColors[i];
+    tmpH = yPrjHours[i];
 
-function chgWorkTypeHour() {
-    
-    tmp = yWorkTypeHours[0];
-    for( i = 0; i < yWorkTypeHours.length - 1; i++) {
-         yWorkTypeHours[i] = yWorkTypeHours[i+1];
+    for (i = yPrjProgress.length - 1; i > 0; i--) {
+        xPrjNames[i] = xPrjNames[i - 1];
+        yPrjProgress[i] = yPrjProgress[i - 1];
+        prjChartColors[i] = prjChartColors[i - 1];
+        yPrjHours[i] = yPrjHours[i - 1];
     }
+
+    xPrjNames[i] = tmpPrj;
+    yPrjProgress[i] = tmpY;
+    prjChartColors[i] = tmpC;
+    yPrjHours[i] = tmpH;
+    if( chartIdName == 'dashboardChart' ) {
+        dashbdChart.update();
+    } else {
+        prjHourChart.update();
+    }
+}
+
+function updatePrjHour() {
+    let sel = document.getElementById('selPrj4Hour');
+    let newVal = document.getElementById('newHour');
+    let ix = sel.selectedIndex - 1;
+    if( ix < xPrjNames.length ) {
+        yPrjHours[ix] =  parseInt(newVal.value.trim());
+
+        prjHourChart.update();
+    } else {
+        alert("invalid number!")
+    }
+}
+
+
+// function shiftPrjHour() {
+//     let i = xPrjNames.length - 1;
+//     tmpPrj = xPrjNames[i];
+//     tmpH = yPrjHours[i];
+
+//     for (; i > 0; i--) {
+//         xPrjNames[i] = xPrjNames[i - 1];
+//         yPrjHours[i] = yPrjHours[i - 1];
+//         yPrjProgress[i] = yPrjProgress[i - 1];
+//     }
+
+//     xPrjNames[i] = tmpPrj;
+//     yPrjHours[i] = tmpH;
+//     yPrjProgress[i] = tmpY;
+//     prjHourChart.update();
+// }
+
+
+function showSelectedPrjWorkType(me) {
+    let ix = me.selectedIndex;
     
-    yWorkTypeHours[i] = tmp;
+    populateWorkTypeHours(projects[ix].name);
+    wTypeChart.config.options.title.text = "Hours of '" + projects[ix].name + "' Project's Work Type";
+    wTypeChart.update();
+}
+
+function updateWTypeHour() {
+    let prj = document.getElementById('selPrj4WT');
+    let sel = document.getElementById('selWType');
+    let newVal = document.getElementById('newWTHour');
+    let ix = sel.selectedIndex - 1;
+    let prjIx, sum = 0;
+
+    projects.forEach(function(item,index) {
+        if (item.name == prj.value) prjIx = index;
+    });
+
+    if( ix < yWorkTypeHours.length ) {
+        yWorkTypeHours[ix] =  parseInt(newVal.value.trim());
+        projects[prjIx][sel.value] = yWorkTypeHours[ix];
+
+        
+        for( let i = 0; i < yWorkTypeHours.length; i++) {
+            sum += yWorkTypeHours[i];
+        }
+
+        yPrjHours[prjIx] = sum;
+        //populateWorkTypeHours(projects[prjIx].name);
+        document.getElementById('workHours').children[0].textContent = sum;
+        wTypeChart.update();
+        prjHourChart.update();
+    } else {
+        alert("invalid number!")
+    }
+}
+
+function shiftWorkTypeHour() {
+    let i = xWorkTypeNames.length - 1;
+    tmpType = xWorkTypeNames[i];
+    tmpColor = wTypebarColors[i];
+    tmpY = yWorkTypeHours[i];
+
+    for (i = xWorkTypeNames.length - 1; i > 0; i--) {
+        xWorkTypeNames[i] = xWorkTypeNames[i - 1];
+        wTypebarColors[i] = wTypebarColors[i - 1];
+        yWorkTypeHours[i] = yWorkTypeHours[i - 1];
+    }
+
+    xWorkTypeNames[i] = tmpType;
+    wTypebarColors[i] = tmpColor;
+    yWorkTypeHours[i] = tmpY;
     
-    //wTypeChart.config.data.labels[0] = 'Korea';
+    //wTypeChart.config.data.labels[0] = 'Meeting';
     wTypeChart.update();
     //wTypeChart.chart.width = 900;
     //wTypeChart.chart.height = 900;
---
+++ project.js
@@ -0,0 +1,11 @@
+let projects = [
+    {"name":"Dashboard",         "color":"red",    "progress":100, "kick-off":"Dec. 18, 2021", "Design":2,   "Development":7, "Test & Debug":3, "Change Request":0, "Customer Service":0},
+    {"name":"Game",              "color":"orange", "progress":1,   "kick-off":"Dec. 18, 2021", "Design":0.1, "Development":0, "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"IOT",               "color":"cyan",   "progress":1,   "kick-off":"Dec. 18, 2021", "Design":0.1, "Development":0, "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"Bug Tracker",       "color":"green",  "progress":1,   "kick-off":"Dec. 18, 2021", "Design":0.1, "Development":0, "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"Blog",              "color":"brown",  "progress":1,   "kick-off":"Dec. 18, 2021", "Design":0.1, "Development":0, "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"Financial Portal",  "color":"blue",   "progress":1,   "kick-off":"Dec. 18, 2021", "Design":0.1, "Development":0, "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"Inventory Control", "color":"navy",   "progress":1,   "kick-off":"Dec. 18, 2021", "Design":0.1, "Development":0, "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"Work Order System", "color":"purple", "progress":1,   "kick-off":"Dec. 18, 2021", "Design":0.1, "Development":0, "Test & Debug":0, "Change Request":0, "Customer Service":0}
+];
+
